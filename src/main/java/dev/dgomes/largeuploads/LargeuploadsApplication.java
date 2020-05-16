7469dc075dd6a558b76df5f036b62439197cd6e8
Diogo Gomes <diogo.gomes77@gmail.com>
2020-05-16 12:03:48 +0100

    spring initializr

---
+++ src/main/java/dev/dgomes/largeuploads/LargeuploadsApplication.java
@@ -0,0 +1,13 @@
+package dev.dgomes.largeuploads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeuploadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeuploadsApplication.class, args);
+	}
+
+}
