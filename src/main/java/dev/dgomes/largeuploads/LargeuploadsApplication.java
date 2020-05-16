3011210e1367618e3602c4ce7df0911d578291fc
Diogo Gomes <diogo.gomes77@gmail.com>
2020-05-16 12:22:06 +0100

    working large file uploads experience

--- src/main/java/dev/dgomes/largeuploads/LargeuploadsApplication.java
+++ src/main/java/dev/dgomes/largeuploads/LargeuploadsApplication.java
@@ -1,9 +1,15 @@
 package dev.dgomes.largeuploads;
 
+import dev.dgomes.largeuploads.fileupload.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		//FileStorageProperties.class,
+		StorageProperties.class
+})
 public class LargeuploadsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dev/dgomes/largeuploads/fileupload/StorageException.java
@@ -0,0 +1,12 @@
+package dev.dgomes.largeuploads.fileupload;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
